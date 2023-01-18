a40e5ae89f74f6d37298f5234f7131d4311da1a2
alekspanteli <alekspanteli@rambler.ru>
2023-01-18 15:19:22 +0000

    wip challenge

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 function HomePage() {
-  return <h1 className="text-3xl font-bold underline">
-  Hello world!
-</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="card">test</div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
