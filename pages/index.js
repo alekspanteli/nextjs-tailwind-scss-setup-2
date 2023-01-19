9fb4542e543b31a345b1e9212a55a4e91c7dd66b
alekspanteli <alekspanteli@rambler.ru>
2023-01-19 09:55:10 +0000

    updat namespaces

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function HomePage() {
   return (
-    <>
+    <div className="container">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="card">test</div>
-    </>
+      <div className="card">Vertaald uit het Engels-In uitgeverijen en grafisch ontwerp is Lorem ipsum een ​​plaatsvervangende tekst die vaak wordt gebruikt om de visuele vorm van een document of een lettertype te demonstreren zonder te vertrouwen op zinvolle inhoud. Lorem ipsum kan als tijdelijke aanduiding worden gebruikt voordat de definitieve kopie beschikbaar</div>
+    </div>
   );
 }
 
